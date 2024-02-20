3eb63b65b9de0b1387d158eb4ab52919772eb2f8
Warittorn Cheevachaipimol <warittorn@bandprotocol.com>
2024-02-20 18:11:30 +0700

    cleanup + fix minor bug

--- bothan-coin-gecko/src/api/rest.rs
+++ bothan-coin-gecko/src/api/rest.rs
@@ -16,7 +16,7 @@ impl CoinGeckoRestAPI {
     }
 
     pub async fn get_coins_list(&self) -> Result<Vec<Coin>, Error> {
-        let url = format!("{}/coins/list", self.url);
+        let url = format!("{}coins/list", self.url);
         let builder = self.client.get(url);
         let response = send_request(builder).await?;
 
@@ -24,31 +24,32 @@ impl CoinGeckoRestAPI {
     }
 
     pub async fn get_coins_market(&self, ids: &[&str]) -> Vec<Result<Market, Error>> {
-        let ids_jobs = ids
+        let ids_per_pages = ids
             .chunks(MAX_PAGE_SIZE)
             .enumerate()
             .collect::<Vec<(usize, &[&str])>>();
 
-        let url = format!("{}/coins/markets", self.url);
+        let url = format!("{}coins/markets", self.url);
         let base_params = vec![
             ("vs_currency", "usd".to_string()),
             ("per_page", MAX_PAGE_SIZE.to_string()),
+            ("ids", ids.join(",")),
         ];
 
         let mut markets = Vec::with_capacity(ids.len());
-        for (page, ids) in ids_jobs {
+        for (page, page_ids) in ids_per_pages {
             let mut params = base_params.clone();
             params.push(("page", (page + 1).to_string()));
-            params.push(("ids", ids.join(",")));
 
             let builder_with_query = self.client.get(&url).query(&params);
             let market_data = match send_request(builder_with_query).await {
                 Ok(response) => {
-                    if let Ok(markets) = parse_response::<Vec<Market>>(response).await {
+                    if let Ok(markets) = response.json::<Vec<Market>>().await {
                         let map: HashMap<String, Market> =
                             HashMap::from_iter(markets.into_iter().map(|m| (m.id.clone(), m)));
                         // Not found error
-                        ids.iter()
+                        page_ids
+                            .iter()
                             .map(|id| {
                                 let val = map.get(*id).cloned();
                                 if let Some(market) = val {
@@ -59,22 +60,15 @@ impl CoinGeckoRestAPI {
                             })
                             .collect::<Vec<Result<Market, Error>>>()
                     } else {
-                        ids.iter().map(|_| Err(Error::CatchAll)).collect()
+                        page_ids.iter().map(|_| Err(Error::CatchAll)).collect()
                     }
                 }
-                Err(_) => ids.iter().map(|_| Err(Error::CatchAll)).collect(),
+                Err(_) => page_ids.iter().map(|_| Err(Error::CatchAll)).collect(),
             };
             markets.extend(market_data);
         }
         markets
     }
-}
-
-async fn parse_response<T>(response: Response) -> Result<T, Error>
-where
-    T: serde::de::DeserializeOwned,
-{
-    Ok(response.json::<T>().await?)
 }
 
 async fn send_request(request_builder: RequestBuilder) -> Result<Response, Error> {
