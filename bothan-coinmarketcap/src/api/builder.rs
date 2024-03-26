30f03e54e9f08db427a689cb1cc61ac827a92fd1
warittornc <69615455+warittornc@users.noreply.github.com>
2024-03-26 15:06:55 +0700

    [feat] Add CoinMarketCap (#24)
    * add cmc
    
    * [feat] Add HTX (#25)
    
    * add htx
    
    * fixed builder syntax
    
    * fixed builder syntax
    
    * [feat] Bothan Cryptocompare (#16)
    
    * init bothan cryptocompare
    
    * remove test
    
    * remove debug
    
    * update map
    
    * update rest builder
    
    * fix review
    
    * remove unused error
    
    * refactor
    
    * fix test
    
    * fix review
    
    * fix
    
    * remove url
    
    * fix type
    
    * fix now
    
    * update
    
    * remove comment
    
    * fix
    
    * reorder
    
    * fix
    
    ---------
    
    Co-authored-by: satawatnack <satawat@bandprotocol.com>
    Co-authored-by: Kitipong Sirirueangsakul <kitipong.sirir@gmail.com>
    
    * add cmc
    
    * fixed builder syntax
    
    ---------
    
    Co-authored-by: satawat thitisupakul <s.nack1919@gmail.com>
    Co-authored-by: satawatnack <satawat@bandprotocol.com>
    Co-authored-by: Kitipong Sirirueangsakul <kitipong.sirir@gmail.com>

---
+++ bothan-coinmarketcap/src/api/builder.rs
@@ -0,0 +1,52 @@
+use reqwest::header::{HeaderMap, HeaderValue};
+use reqwest::ClientBuilder;
+use url::Url;
+
+use crate::api::error::BuilderError as Error;
+use crate::api::types::DEFAULT_URL;
+use crate::api::CoinMarketCapRestAPI;
+
+pub struct CoinMarketCapRestAPIBuilder {
+    url: String,
+    api_key: Option<String>,
+}
+
+impl CoinMarketCapRestAPIBuilder {
+    pub fn with_url(&mut self, url: &str) -> &Self {
+        self.url = url.into();
+        self
+    }
+
+    pub fn with_api_key(&mut self, api_key: &str) -> &Self {
+        self.api_key = Some(api_key.into());
+        self
+    }
+
+    pub fn build(self) -> Result<CoinMarketCapRestAPI, Error> {
+        let mut headers = HeaderMap::new();
+
+        let parsed_url = Url::parse(&self.url)?;
+
+        let key = match &self.api_key {
+            Some(key) => key,
+            None => return Err(Error::MissingAPIKey()),
+        };
+
+        let mut val = HeaderValue::from_str(key)?;
+        val.set_sensitive(true);
+        headers.insert("X-CMC_PRO_API_KEY", val);
+
+        let client = ClientBuilder::new().default_headers(headers).build()?;
+
+        Ok(CoinMarketCapRestAPI::new(parsed_url, client))
+    }
+}
+
+impl Default for CoinMarketCapRestAPIBuilder {
+    fn default() -> Self {
+        CoinMarketCapRestAPIBuilder {
+            url: DEFAULT_URL.into(),
+            api_key: None,
+        }
+    }
+}
---
+++ bothan-coinmarketcap/src/service/builder.rs
@@ -0,0 +1,65 @@
+use tokio::time::Duration;
+
+use crate::api::error::BuilderError;
+use crate::api::CoinMarketCapRestAPIBuilder;
+use crate::CoinMarketCapService;
+
+pub(crate) const DEFAULT_UPDATE_INTERVAL: Duration = Duration::from_secs(60);
+pub(crate) const DEFAULT_UPDATE_SUPPORTED_ASSETS_INTERVAL: Duration = Duration::from_secs(86400);
+
+pub struct CoinMarketCapServiceBuilder {
+    url: Option<String>,
+    api_key: Option<String>,
+    update_interval: Duration,
+    update_supported_assets_interval: Duration,
+}
+
+impl CoinMarketCapServiceBuilder {
+    pub fn with_url(mut self, url: &str) -> Self {
+        self.url = Some(url.into());
+        self
+    }
+
+    pub fn with_api_key(mut self, api_key: &str) -> Self {
+        self.api_key = Some(api_key.into());
+        self
+    }
+    pub fn with_update_interval(mut self, update_interval: Duration) -> Self {
+        self.update_interval = update_interval;
+        self
+    }
+
+    pub fn with_update_supported_assets_interval(
+        mut self,
+        update_supported_assets_interval: Duration,
+    ) -> Self {
+        self.update_supported_assets_interval = update_supported_assets_interval;
+        self
+    }
+
+    pub async fn build(self) -> Result<CoinMarketCapService, BuilderError> {
+        let mut api_builder = CoinMarketCapRestAPIBuilder::default();
+        if let Some(url) = &self.url {
+            api_builder.with_url(url);
+        };
+        if let Some(api_key) = &self.api_key {
+            api_builder.with_api_key(api_key);
+        };
+        let api = api_builder.build()?;
+
+        let service = CoinMarketCapService::new(api, self.update_interval).await;
+
+        Ok(service)
+    }
+}
+
+impl Default for CoinMarketCapServiceBuilder {
+    fn default() -> Self {
+        CoinMarketCapServiceBuilder {
+            url: None,
+            api_key: None,
+            update_interval: DEFAULT_UPDATE_INTERVAL,
+            update_supported_assets_interval: DEFAULT_UPDATE_SUPPORTED_ASSETS_INTERVAL,
+        }
+    }
+}
