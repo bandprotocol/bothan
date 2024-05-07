f149d00768637e1b165ca8df791dc42076ea10e6
warittornc <69615455+warittornc@users.noreply.github.com>
2024-05-07 15:46:05 +0700

    [chore] Add Binance Docs (#36)
    * add docs
    
    * minor fix and refactor
    
    * add docs
    
    * minor fix and refactor
    
    * fix
    
    * fix

--- bothan-binance/src/service/builder.rs
+++ bothan-binance/src/service/builder.rs
@@ -8,6 +8,7 @@ use crate::error::Error;
 use crate::types::DEFAULT_CHANNEL_SIZE;
 use crate::{BinanceService, BinanceWebSocketConnector};
 
+/// Options for the `BinanceServiceBuilder`.
 #[derive(Clone, Debug, Deserialize)]
 pub struct BinanceServiceBuilderOpts {
     pub url: Option<String>,
@@ -15,6 +16,25 @@ pub struct BinanceServiceBuilderOpts {
     pub remove_id_ch_size: Option<usize>,
 }
 
+/// Builds a Binance service with custom options.
+/// Methods can be chained to set the configuration values and the
+/// service is constructed by calling the [`build`](BinanceServiceBuilder::build) method.
+/// # Example
+/// ```no_run
+/// use bothan_binance::BinanceServiceBuilder;
+///
+/// #[tokio::main]
+/// async fn main() {
+///     let service = BinanceServiceBuilder::default()
+///         .with_cmd_ch_size(100)
+///         .with_remove_id_ch_size(100)
+///         .build()
+///         .await
+///         .unwrap();
+///
+///     // use service ...
+/// }
+/// ```
 pub struct BinanceServiceBuilder {
     url: String,
     cmd_ch_size: usize,
@@ -22,21 +42,7 @@ pub struct BinanceServiceBuilder {
 }
 
 impl BinanceServiceBuilder {
-    pub fn with_url(mut self, url: String) -> Self {
-        self.url = url;
-        self
-    }
-
-    pub fn with_cmd_ch_size(mut self, size: usize) -> Self {
-        self.cmd_ch_size = size;
-        self
-    }
-
-    pub fn with_remove_id_ch_size(mut self, size: usize) -> Self {
-        self.remove_id_ch_size = size;
-        self
-    }
-
+    /// Returns a new `BinanceServiceBuilder` with the given options.
     pub fn new(opts: BinanceServiceBuilderOpts) -> Self {
         Self {
             url: opts.url.unwrap_or(DEFAULT_URL.to_string()),
@@ -45,6 +51,28 @@ impl BinanceServiceBuilder {
         }
     }
 
+    /// Set the URL for the Binance service.
+    /// The default URL is `DEFAULT_URL`.
+    pub fn with_url(mut self, url: String) -> Self {
+        self.url = url;
+        self
+    }
+
+    /// Set the internal command channel size for the Binance service.
+    /// The default size is `DEFAULT_CHANNEL_SIZE`.
+    pub fn with_cmd_ch_size(mut self, size: usize) -> Self {
+        self.cmd_ch_size = size;
+        self
+    }
+
+    /// Set the internal remove ID channel size for the Binance service.
+    /// The default size is `DEFAULT_CHANNEL_SIZE`.
+    pub fn with_remove_id_ch_size(mut self, size: usize) -> Self {
+        self.remove_id_ch_size = size;
+        self
+    }
+
+    /// Creates the configured `BinanceService`.
     pub async fn build(self) -> Result<BinanceService, Error> {
         let connector = BinanceWebSocketConnector::new(self.url);
         let connection = connector.connect().await?;
@@ -59,6 +87,7 @@ impl BinanceServiceBuilder {
 }
 
 impl Default for BinanceServiceBuilder {
+    /// Create a new `BinanceServiceBuilder` with the default values.
     fn default() -> Self {
         Self {
             url: DEFAULT_URL.to_string(),
