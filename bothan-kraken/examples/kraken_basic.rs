0a6793e4779eb4b6b6b6a2d405e96e4b138fe50f
Warittorn Cheevachaipimol <warittorn@bandprotocol.com>
2024-05-21 10:58:11 +0700

    cleanup

--- bothan-kraken/examples/kraken_basic.rs
+++ bothan-kraken/examples/kraken_basic.rs
@@ -10,9 +10,9 @@ async fn main() {
     let service = KrakenServiceBuilder::default().build().await;
 
     if let Ok(mut service) = service {
-        let data = service.get_price_data(&["BTC/USD", "ETH/USD"]).await;
-        println!("{:?}", data);
         loop {
+            let data = service.get_price_data(&["BTC/USD", "ETH/USD"]).await;
+            println!("{:?}", data);
             tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
         }
     }
