c81d94b30492a3302919d68d1468e25130d4f869
Warittorn Cheevachaipimol <warittorn@bandprotocol.com>
2024-07-05 01:42:16 +0700

    added more descriptive doc for builder opts

--- bothan-binance/src/worker/builder.rs
+++ bothan-binance/src/worker/builder.rs
@@ -12,7 +12,11 @@ use crate::worker::types::DEFAULT_CHANNEL_SIZE;
 use crate::worker::BinanceWorker;
 use crate::BinanceWebSocketConnector;
 
-/// Options for the `BinanceServiceBuilder`.
+/// Options for configuring the `BinanceWorkerBuilder`.
+///
+/// `BinanceWorkerBuilderOpts` provides a way to specify custom settings for creating a `BinanceWorker`.
+/// This struct allows users to set optional parameters such as the WebSocket URL and the internal channel size,
+/// which will be used during the construction of the `BinanceWorker`.
 #[derive(Clone, Debug, Default, Deserialize)]
 pub struct BinanceWorkerBuilderOpts {
     pub url: Option<String>,
