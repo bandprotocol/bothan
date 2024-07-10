c29a7db4dbef07b51c51341e106691a5abda6403
Warittorn Cheevachaipimol <warittorn@bandprotocol.com>
2024-07-10 14:56:13 +0700

    cleanup

--- bothan-coingecko/src/worker/error.rs
+++ bothan-coingecko/src/worker/error.rs
@@ -3,7 +3,6 @@ pub enum ParseError {
     #[error("invalid price: {0}")]
     InvalidPrice(f64),
 
-    /// The provided timestamp is invalid.
     #[error("invalid timestamp: {0}")]
     InvalidTimestamp(#[from] chrono::ParseError),
 }
