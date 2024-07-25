15093345e79cc7b02f57c74920906786fabdd185
colmazia <ongart.pisan@gmail.com>
2024-07-25 17:17:09 +0700

    delete unnecessary files

--- bothan-okx/src/types.rs
+++
@@ -1,8 +0,0 @@
-use std::time::Duration;
-
-pub const DEFAULT_CHANNEL_SIZE: usize = 100;
-pub const DEFAULT_TIMEOUT: Duration = Duration::from_secs(720);
-
-pub(crate) enum Command {
-    Subscribe(Vec<String>),
-}
