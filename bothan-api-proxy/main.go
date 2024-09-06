13c08ecb69c50ad0158365b5f9e0d69c7aa1383b
Warittorn Cheevachaipimol <warittorn@bandprotocol.com>
2024-09-06 15:46:15 +0700

    patch proxy to use new prot

--- bothan-api-proxy/main.go
+++ bothan-api-proxy/main.go
@@ -11,7 +11,8 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/grpclog"
 
-	"github.com/bandprotocol/bothan/bothan-api/client/go-client/query"
+	"github.com/bandprotocol/bothan/bothan-api/client/go-client/proto/price"
+	"github.com/bandprotocol/bothan/bothan-api/client/go-client/proto/signal"
 )
 
 type GrpcConfig struct {
@@ -28,17 +29,23 @@ func run(grpcConfig GrpcConfig, goProxyConfig GoProxyConfig) error {
 	defer cancel()
 
 	// Register gRPC server endpoint
-	// Note: Make sure the gRPC server is running properly and accessible
+	// Note: Make sure the gRPC server is running properly and accessibly
 	mux := runtime.NewServeMux()
 	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
-	err := query.RegisterQueryHandlerFromEndpoint(ctx, mux, grpcConfig.Addr, opts)
+
+	err := signal.RegisterSignalServiceHandlerFromEndpoint(ctx, mux, grpcConfig.Addr, opts)
+	if err != nil {
+		return err
+	}
+
+	err = price.RegisterPriceServiceHandlerFromEndpoint(ctx, mux, grpcConfig.Addr, opts)
 	if err != nil {
 		return err
 	}
 
 	fmt.Println("Server running on", goProxyConfig.Addr)
 
-	// Start HTTP server (and proxy calls to gRPC server endpoint)
+	// Start an HTTP server (and proxy calls to gRPC server endpoint)
 	return http.ListenAndServe(goProxyConfig.Addr, mux)
 }
 
