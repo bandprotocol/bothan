e4235a78877d2e24450b7f0feac63964d18299d8
Ongart Pisansathienwong <ongart.pisan@gmail.com>
2024-09-25 13:51:07 +0700

    [!feat] Refactor Coinbase (#65)
    * minor changes
    
    * [!feat] Update Binance (#58)
    
    * modify binance
    
    * minor change
    
    * fix
    
    * minor changes and add doc
    
    * remove unnecessary traits
    
    * simplify to single store
    
    * simplify to single store
    
    * refactor
    
    * remove anyhow and cleanup
    
    * fix
    
    * fix comment and cleanup
    
    * minor move
    
    * fix doc
    
    * fix comments
    
    * fix and format
    
    * add ids to log
    
    * rename set to add
    
    * added more descriptive doc for builder opts
    
    * fixed typo
    
    * fixed
    
    * add disconnect on exit
    
    * cleanup
    
    * grammer fix
    
    * add verbose error
    
    * change type from T to K
    
    * format
    
    * fix
    
    * cleanup
    
    * fix comments
    
    * change error to warn
    
    * [!feat] Refactor CoinGecko (#60)
    
    * [!feat] Minor Specification Changes  (#62)
    
    * update proto
    
    * minor changes
    
    * regenerate proto
    
    * [!feat] Refactor Kraken (#61)
    
    * refactor kraken
    
    * add more comments
    
    * store multiple assetinfos at once, add unit tests
    
    * fix worker type spec
    
    * implement coinbase refactoring
    
    * add unit tests
    
    * [!feat] Refactor API (#63)
    
    refactor api
    
    * update coinbase to match new bothan
    
    * fix format
    
    * fix merging bug
    
    * fix merged files
    
    * fix from comments in update-okx
    
    * fix format
    
    * change default timeout
    
    * fix from comments
    
    * fix websocket connection error
    
    * fix decimal error name
    
    ---------
    
    Co-authored-by: Warittorn Cheevachaipimol <warittorn@bandprotocol.com>
    Co-authored-by: warittornc <69615455+warittornc@users.noreply.github.com>

---
+++ bothan-coinbase/src/worker/asset_worker.rs
@@ -0,0 +1,241 @@
+use std::sync::Weak;
+
+use rust_decimal::Decimal;
+use tokio::select;
+use tokio::sync::mpsc::Receiver;
+use tokio::time::{sleep, timeout};
+use tracing::{debug, error, info, warn};
+
+use bothan_core::store::WorkerStore;
+use bothan_core::types::AssetInfo;
+
+use crate::api::error::{MessageError, SendError};
+use crate::api::types::channels::Channel;
+use crate::api::types::CoinbaseResponse;
+use crate::api::{CoinbaseWebSocketConnection, CoinbaseWebSocketConnector, Ticker};
+use crate::worker::error::WorkerError;
+use crate::worker::types::{DEFAULT_TIMEOUT, RECONNECT_BUFFER};
+use crate::worker::CoinbaseWorker;
+
+pub(crate) async fn start_asset_worker(
+    worker: Weak<CoinbaseWorker>,
+    mut connection: CoinbaseWebSocketConnection,
+    mut subscribe_rx: Receiver<Vec<String>>,
+    mut unsubscribe_rx: Receiver<Vec<String>>,
+) {
+    loop {
+        select! {
+            Some(ids) = subscribe_rx.recv() => handle_subscribe_recv(ids, &mut connection).await,
+            Some(ids) = unsubscribe_rx.recv() => handle_unsubscribe_recv(ids, &mut connection).await,
+            result = timeout(DEFAULT_TIMEOUT, connection.next()) => {
+                if let Some(worker) = worker.upgrade() {
+                    match result {
+                        Err(_) => handle_reconnect(&worker.connector, &mut connection, &worker.store).await,
+                        Ok(coinbase_result) => handle_connection_recv(coinbase_result, &worker.connector, &mut connection, &worker.store).await,
+                    }
+                } else {
+                    break
+                }
+            }
+        }
+    }
+
+    // Close the connection upon exiting
+    if let Err(e) = connection.close().await {
+        error!("asset worker failed to send close frame: {}", e)
+    } else {
+        debug!("asset worker successfully sent close frame")
+    }
+
+    debug!("asset worker has been dropped, stopping asset worker");
+}
+
+async fn subscribe(
+    ids: &[String],
+    connection: &mut CoinbaseWebSocketConnection,
+) -> Result<(), SendError> {
+    if !ids.is_empty() {
+        let ids_vec = ids.iter().map(|s| s.as_str()).collect::<Vec<&str>>();
+        connection
+            .subscribe(vec![Channel::Ticker], &ids_vec)
+            .await?
+    }
+
+    Ok(())
+}
+
+async fn handle_subscribe_recv(ids: Vec<String>, connection: &mut CoinbaseWebSocketConnection) {
+    if let Err(e) = subscribe(&ids, connection).await {
+        error!("failed to subscribe to ids {:?}: {}", ids, e);
+    } else {
+        info!("subscribed to ids {:?}", ids);
+    }
+}
+
+async fn unsubscribe(
+    ids: &[String],
+    connection: &mut CoinbaseWebSocketConnection,
+) -> Result<(), SendError> {
+    if !ids.is_empty() {
+        connection
+            .unsubscribe(
+                vec![Channel::Ticker],
+                &ids.iter().map(|s| s.as_str()).collect::<Vec<&str>>(),
+            )
+            .await?
+    }
+
+    Ok(())
+}
+
+async fn handle_unsubscribe_recv(ids: Vec<String>, connection: &mut CoinbaseWebSocketConnection) {
+    if let Err(e) = unsubscribe(&ids, connection).await {
+        error!("failed to unsubscribe to ids {:?}: {}", ids, e);
+    } else {
+        info!("unsubscribed to ids {:?}", ids);
+    }
+}
+
+async fn handle_reconnect(
+    connector: &CoinbaseWebSocketConnector,
+    connection: &mut CoinbaseWebSocketConnection,
+    query_ids: &WorkerStore,
+) {
+    let mut retry_count: usize = 1;
+    loop {
+        warn!("reconnecting: attempt {}", retry_count);
+
+        if let Ok(new_connection) = connector.connect().await {
+            *connection = new_connection;
+
+            // Resubscribe to all ids
+            let Ok(ids) = query_ids.get_query_ids().await else {
+                error!("failed to get query ids from store");
+                return;
+            };
+
+            let ids_vec = ids.into_iter().collect::<Vec<String>>();
+
+            match subscribe(&ids_vec, connection).await {
+                Ok(_) => {
+                    info!("resubscribed to all ids");
+                    return;
+                }
+                Err(_) => {
+                    error!("failed to resubscribe to all ids");
+                }
+            }
+        } else {
+            error!("failed to reconnect to coinbase");
+        }
+
+        retry_count += 1;
+        sleep(RECONNECT_BUFFER).await;
+    }
+}
+
+fn parse_ticker(ticker: &Ticker) -> Result<AssetInfo, WorkerError> {
+    let id = ticker.product_id.clone();
+    let price_value = Decimal::from_str_exact(&ticker.price)?;
+    let timestamp = chrono::Utc::now().timestamp();
+    Ok(AssetInfo::new(id, price_value, timestamp))
+}
+
+async fn store_ticker(store: &WorkerStore, ticker: &Ticker) -> Result<(), WorkerError> {
+    let id = ticker.product_id.clone();
+    store.set_asset(id, parse_ticker(ticker)?).await?;
+    Ok(())
+}
+
+/// Processes the response from the Coinbase API.
+async fn process_response(resp: CoinbaseResponse, store: &WorkerStore) {
+    match resp {
+        CoinbaseResponse::Ticker(ticker) => match store_ticker(store, &ticker).await {
+            Ok(_) => info!("saved data"),
+            Err(e) => error!("failed to save data: {}", e),
+        },
+        CoinbaseResponse::Subscriptions(_) => {
+            info!("received request response");
+        }
+    }
+}
+
+async fn handle_connection_recv(
+    recv_result: Result<CoinbaseResponse, MessageError>,
+    connector: &CoinbaseWebSocketConnector,
+    connection: &mut CoinbaseWebSocketConnection,
+    store: &WorkerStore,
+) {
+    match recv_result {
+        Ok(resp) => {
+            process_response(resp, store).await;
+        }
+        Err(MessageError::ChannelClosed) => {
+            handle_reconnect(connector, connection, store).await;
+        }
+        Err(MessageError::UnsupportedMessage) => {
+            error!("unsupported message received from coinbase");
+        }
+        Err(MessageError::Parse(..)) => {
+            error!("unable to parse message from coinbase");
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse_market() {
+        let ticker = Ticker {
+            sequence: 1,
+            product_id: "BTC-USD".to_string(),
+            price: "42000.99".to_string(),
+            open_24h: "9000.00".to_string(),
+            volume_24h: "1000.00".to_string(),
+            low_24h: "9500.00".to_string(),
+            high_24h: "10500.00".to_string(),
+            volume_30d: "30000.00".to_string(),
+            best_bid: "9999.00".to_string(),
+            best_bid_size: "0.01".to_string(),
+            best_ask: "10001.00".to_string(),
+            best_ask_size: "0.01".to_string(),
+            side: "buy".to_string(),
+            time: "2021-01-01T00:00:00.000Z".to_string(),
+            trade_id: 1,
+            last_size: "0.01".to_string(),
+        };
+        let result = parse_ticker(&ticker);
+        let expected = AssetInfo::new(
+            "BTC-USD".to_string(),
+            Decimal::from_str_exact("42000.99").unwrap(),
+            0,
+        );
+        assert_eq!(result.as_ref().unwrap().id, expected.id);
+        assert_eq!(result.unwrap().price, expected.price);
+    }
+
+    #[test]
+    fn test_parse_market_with_failure() {
+        let ticker = Ticker {
+            sequence: 1,
+            product_id: "BTC-USD".to_string(),
+            price: f64::INFINITY.to_string(),
+            open_24h: "9000.00".to_string(),
+            volume_24h: "1000.00".to_string(),
+            low_24h: "9500.00".to_string(),
+            high_24h: "10500.00".to_string(),
+            volume_30d: "30000.00".to_string(),
+            best_bid: "9999.00".to_string(),
+            best_bid_size: "0.01".to_string(),
+            best_ask: "10001.00".to_string(),
+            best_ask_size: "0.01".to_string(),
+            side: "buy".to_string(),
+            time: "2021-01-01T00:00:00.000Z".to_string(),
+            trade_id: 1,
+            last_size: "0.01".to_string(),
+        };
+        assert!(parse_ticker(&ticker).is_err());
+    }
+}
