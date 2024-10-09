5d716879c55b87952b90c3b4f183bca15838e3cc
Warittorn Cheevachaipimol <warittorn@bandprotocol.com>
2024-10-09 14:28:04 +0700

    modify cryptocompare opts

--- bothan-cryptocompare/src/worker/opts.rs
+++ bothan-cryptocompare/src/worker/opts.rs
@@ -1,4 +1,4 @@
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 use crate::api::DEFAULT_URL;
 use crate::worker::types::DEFAULT_CHANNEL_SIZE;
@@ -13,6 +13,8 @@ pub struct CryptoCompareWorkerBuilderOpts {
     #[serde(default = "default_url")]
     pub url: String,
     #[serde(default)]
+    #[serde(deserialize_with = "empty_string_is_none")]
+    #[serde(serialize_with = "none_is_empty_string")]
     pub api_key: Option<String>,
     #[serde(default = "default_internal_ch_size")]
     pub internal_ch_size: usize,
@@ -33,5 +35,22 @@ impl Default for CryptoCompareWorkerBuilderOpts {
             api_key: None,
             internal_ch_size: default_internal_ch_size(),
         }
+    }
+}
+
+fn empty_string_is_none<'de, D: Deserializer<'de>>(
+    deserializer: D,
+) -> Result<Option<String>, D::Error> {
+    let s: Option<String> = Option::deserialize(deserializer)?;
+    Ok(s.filter(|s| !s.is_empty()))
+}
+
+fn none_is_empty_string<S: Serializer>(
+    value: &Option<String>,
+    serializer: S,
+) -> Result<S::Ok, S::Error> {
+    match value {
+        Some(val) => serializer.serialize_str(val),
+        None => serializer.serialize_str(""),
     }
 }
