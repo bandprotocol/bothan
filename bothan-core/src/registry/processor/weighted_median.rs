dda108097f982872e5f835a68e9a6fc8d45ef621
Warittorn Cheevachaipimol <warittorn@bandprotocol.com>
2024-10-15 14:35:55 +0700

    change weights to u32

--- bothan-core/src/registry/processor/weighted_median.rs
+++ bothan-core/src/registry/processor/weighted_median.rs
@@ -2,10 +2,7 @@ use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::ops::{Add, Div};
 
-use bincode::de::{BorrowDecoder, Decoder};
-use bincode::enc::Encoder;
-use bincode::error::{DecodeError, EncodeError};
-use bincode::{BorrowDecode, Decode, Encode};
+use bincode::{Decode, Encode};
 use num_traits::{FromPrimitive, Zero};
 use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
@@ -17,65 +14,15 @@ use crate::registry::processor::{Process, ProcessError};
 /// minimum cumulative weight required to calculate the weighted median. If the cumulative weight
 /// of the data sources is less than `minimum_cumulative_weight` or the source associated with the
 /// data does not have an assigned weight, it returns an error.
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize, Encode, Decode)]
 pub struct WeightedMedianProcessor {
-    pub source_weights: HashMap<String, Decimal>,
-    pub minimum_cumulative_weight: Decimal,
-}
-
-impl Encode for WeightedMedianProcessor {
-    fn encode<E: Encoder>(&self, encoder: &mut E) -> Result<(), EncodeError> {
-        let encodable_map = self
-            .source_weights
-            .iter()
-            .map(|(k, v)| (k, v.serialize()))
-            .collect::<HashMap<&String, [u8; 16]>>();
-        Encode::encode(&encodable_map, encoder)?;
-        Encode::encode(&self.minimum_cumulative_weight.serialize(), encoder)
-    }
-}
-
-impl Decode for WeightedMedianProcessor {
-    fn decode<D: Decoder>(decoder: &mut D) -> Result<Self, DecodeError> {
-        let serialized_decimal_map: HashMap<String, [u8; 16]> = Decode::decode(decoder)?;
-        let source_weights = serialized_decimal_map
-            .into_iter()
-            .map(|(k, v)| (k, Decimal::deserialize(v)))
-            .collect();
-
-        let minimum_cumulative_weight = Decimal::deserialize(Decode::decode(decoder)?);
-
-        Ok(WeightedMedianProcessor {
-            source_weights,
-            minimum_cumulative_weight,
-        })
-    }
-}
-
-impl<'de> BorrowDecode<'de> for WeightedMedianProcessor {
-    fn borrow_decode<D: BorrowDecoder<'de>>(decoder: &mut D) -> Result<Self, DecodeError> {
-        let serialized_decimal_map: HashMap<String, [u8; 16]> =
-            BorrowDecode::borrow_decode(decoder)?;
-        let source_weights = serialized_decimal_map
-            .into_iter()
-            .map(|(k, v)| (k, Decimal::deserialize(v)))
-            .collect();
-
-        let minimum_cumulative_weight = Decimal::deserialize(BorrowDecode::borrow_decode(decoder)?);
-
-        Ok(WeightedMedianProcessor {
-            source_weights,
-            minimum_cumulative_weight,
-        })
-    }
+    pub source_weights: HashMap<String, u32>,
+    pub minimum_cumulative_weight: u32,
 }
 
 impl WeightedMedianProcessor {
     /// Creates a new `WeightedMedianProcessor`.
-    pub fn new(
-        source_weights: HashMap<String, Decimal>,
-        minimum_cumulative_weight: Decimal,
-    ) -> Self {
+    pub fn new(source_weights: HashMap<String, u32>, minimum_cumulative_weight: u32) -> Self {
         WeightedMedianProcessor {
             source_weights,
             minimum_cumulative_weight,
@@ -88,7 +35,7 @@ impl Process<(String, Decimal), Decimal> for WeightedMedianProcessor {
     /// data sources are less than the minimum cumulative weight or the source associated
     /// with the data does not have an assigned weight, it returns an error.
     fn process(&self, data: Vec<(String, Decimal)>) -> Result<Decimal, ProcessError> {
-        let cumulative_weight = data.iter().try_fold(Decimal::zero(), |acc, (source, _)| {
+        let cumulative_weight = data.iter().try_fold(0, |acc, (source, _)| {
             self.source_weights
                 .get(source)
                 .map(|weight| acc + weight)
@@ -109,44 +56,42 @@ impl Process<(String, Decimal), Decimal> for WeightedMedianProcessor {
                     .map(|weight| (value, *weight))
                     .ok_or(ProcessError::new(format!("Unknown source {source}")))
             })
-            .collect::<Result<Vec<(Decimal, Decimal)>, ProcessError>>()?;
+            .collect::<Result<Vec<(Decimal, u32)>, ProcessError>>()?;
 
-        let median = compute_weighted_median(values)
-            .ok_or_else(|| ProcessError::new("Failed to calculate weighted median"))?;
-        Ok(median)
+        Ok(compute_weighted_median(values))
     }
 }
 
-fn compute_weighted_median<T>(mut values: Vec<(T, Decimal)>) -> Option<T>
+// This function requires that values passed is not an empty vector, if an empty vector is passed,
+// it will panic
+fn compute_weighted_median<T>(mut values: Vec<(T, u32)>) -> T
 where
     T: Ord + Add<Output = T> + Div<Output = T> + FromPrimitive,
 {
     values.sort_by(|(v1, _), (v2, _)| v1.cmp(v2));
 
-    let sum = values
+    let mid = values
         .iter()
-        .fold(Decimal::zero(), |acc, (_, weight)| acc + weight);
+        .fold(u32::zero(), |acc, (_, weight)| acc + weight);
 
-    let mid = sum / Decimal::new(2, 0);
-
-    let mut cumulative_weight = Decimal::zero();
+    let mut cumulative_weight = u32::zero();
     let mut iter = values.into_iter();
     while let Some((value, weight)) = iter.next() {
-        cumulative_weight += weight;
+        cumulative_weight += weight * 2;
         match cumulative_weight.cmp(&mid) {
-            Ordering::Greater => return Some(value),
+            Ordering::Greater => return value,
             Ordering::Equal => {
                 return if let Some((next_value, _)) = iter.next() {
-                    Some((value + next_value) / FromPrimitive::from_u32(2).unwrap())
+                    (value + next_value) / FromPrimitive::from_u32(2).unwrap()
                 } else {
-                    Some(value)
+                    value
                 };
             }
             Ordering::Less => (),
         }
     }
 
-    None
+    unreachable!()
 }
 
 #[cfg(test)]
@@ -156,14 +101,14 @@ mod tests {
     #[test]
     fn test_weighted_median() {
         let source_weights = HashMap::from([
-            ("a".to_string(), Decimal::from_str_exact("0.15").unwrap()),
-            ("b".to_string(), Decimal::from_str_exact("0.1").unwrap()),
-            ("c".to_string(), Decimal::from_str_exact("0.2").unwrap()),
-            ("d".to_string(), Decimal::from_str_exact("0.3").unwrap()),
-            ("e".to_string(), Decimal::from_str_exact("0.25").unwrap()),
+            ("a".to_string(), 15),
+            ("b".to_string(), 10),
+            ("c".to_string(), 20),
+            ("d".to_string(), 30),
+            ("e".to_string(), 25),
         ]);
 
-        let weighted_median = WeightedMedianProcessor::new(source_weights, Decimal::zero());
+        let weighted_median = WeightedMedianProcessor::new(source_weights, 0);
         let data = vec![
             ("a".to_string(), Decimal::from(1)),
             ("b".to_string(), Decimal::from(2)),
@@ -178,10 +123,9 @@ mod tests {
 
     #[test]
     fn test_median_with_even_weight() {
-        let source_weights =
-            HashMap::from([("a".to_string(), Decimal::from_str_exact("0.2").unwrap())]);
+        let source_weights = HashMap::from([("a".to_string(), 2)]);
 
-        let weighted_median = WeightedMedianProcessor::new(source_weights, Decimal::zero());
+        let weighted_median = WeightedMedianProcessor::new(source_weights, 0);
         let data = vec![
             ("a".to_string(), Decimal::from(1)),
             ("a".to_string(), Decimal::from(2)),
@@ -197,13 +141,13 @@ mod tests {
     #[test]
     fn test_weighted_median_with_intersect() {
         let source_weights = HashMap::from([
-            ("a".to_string(), Decimal::from_str_exact("0.49").unwrap()),
-            ("b".to_string(), Decimal::from_str_exact("0.01").unwrap()),
-            ("c".to_string(), Decimal::from_str_exact("0.25").unwrap()),
-            ("d".to_string(), Decimal::from_str_exact("0.25").unwrap()),
+            ("a".to_string(), 49),
+            ("b".to_string(), 1),
+            ("c".to_string(), 25),
+            ("d".to_string(), 25),
         ]);
 
-        let weighted_median = WeightedMedianProcessor::new(source_weights, Decimal::zero());
+        let weighted_median = WeightedMedianProcessor::new(source_weights, 0);
         let data = vec![
             ("a".to_string(), Decimal::from(1)),
             ("b".to_string(), Decimal::from(2)),
