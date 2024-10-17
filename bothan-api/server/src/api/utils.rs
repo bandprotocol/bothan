fccc0099ebd3e8e3e8e0eb838e649ff6404ba76d
Warittorn Cheevachaipimol <warittorn@bandprotocol.com>
2024-10-17 16:19:35 +0700

    fix scaling bug

--- bothan-api/server/src/api/utils.rs
+++ bothan-api/server/src/api/utils.rs
@@ -8,14 +8,13 @@ use crate::proto::price::{Price, Status};
 
 pub fn parse_price_state(id: String, price_state: PriceState) -> Price {
     match price_state {
-        PriceState::Available(raw_price) => {
-            let mantissa_price = raw_price
-                .round_dp_with_strategy(PRECISION, RoundingStrategy::ToZero)
-                .mantissa();
-            match u64::try_from(mantissa_price) {
+        PriceState::Available(mut raw_price) => {
+            raw_price.rescale(PRECISION);
+            let mantissa = raw_price.mantissa();
+            match u64::try_from(mantissa) {
                 Ok(p) => Price::new(id, p, Status::Available),
                 Err(_) => {
-                    warn!("failed to convert {mantissa_price} to u64 for id {id}");
+                    warn!("failed to convert {mantissa} to u64 for id {id}");
                     Price::new(id, 0u64, Status::Unavailable)
                 }
             }
