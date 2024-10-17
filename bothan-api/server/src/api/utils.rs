28b10120e076dcf97abd455af028def85c887a97
Warittorn Cheevachaipimol <warittorn@bandprotocol.com>
2024-10-17 19:16:01 +0700

    add unit tests

--- bothan-api/server/src/api/utils.rs
+++ bothan-api/server/src/api/utils.rs
@@ -22,3 +22,51 @@ pub fn parse_price_state(id: String, price_state: PriceState) -> Price {
         PriceState::Unsupported => Price::new(id, 0u64, Status::Unsupported),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use rust_decimal::Decimal;
+
+    use super::*;
+
+    #[test]
+    fn test_parse_price_state() {
+        let id = "test".to_string();
+        let price_state = PriceState::Available(Decimal::from_str_exact("0.123456789").unwrap());
+        let price = parse_price_state(id.clone(), price_state);
+
+        let expected_price = Price::new(id.clone(), 123456789_u64, Status::Available);
+        assert_eq!(price, expected_price);
+    }
+
+    #[test]
+    fn test_parse_price_state_with_lower_scale() {
+        let id = "test".to_string();
+        let price_state = PriceState::Available(Decimal::from_str_exact("0.1").unwrap());
+        let price = parse_price_state(id.clone(), price_state);
+
+        let expected_price = Price::new(id.clone(), 100000000_u64, Status::Available);
+        assert_eq!(price, expected_price);
+    }
+
+    #[test]
+    fn test_parse_price_state_with_higher_scale() {
+        let id = "test".to_string();
+        let price_state =
+            PriceState::Available(Decimal::from_str_exact("0.0010000000000001").unwrap());
+        let price = parse_price_state(id.clone(), price_state);
+
+        let expected_price = Price::new(id.clone(), 1000000_u64, Status::Available);
+        assert_eq!(price, expected_price);
+    }
+
+    #[test]
+    fn test_parse_price_state_with_higher_scale_and_round_up() {
+        let id = "test".to_string();
+        let price_state = PriceState::Available(Decimal::from_str_exact("0.0010000019").unwrap());
+        let price = parse_price_state(id.clone(), price_state);
+
+        let expected_price = Price::new(id.clone(), 1000002_u64, Status::Available);
+        assert_eq!(price, expected_price);
+    }
+}
