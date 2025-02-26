ab183b4304ca9c54e58c9880a8556d5b7fe0f84a
Warittorn Cheevachaipimol <warittorn@bandprotocol.com>
2025-02-26 13:31:14 +0700

    fix

--- bothan-api/server-cli/src/commands/start.rs
+++ bothan-api/server-cli/src/commands/start.rs
@@ -101,7 +101,7 @@ async fn init_rocks_db_store(
         }
         // If the path does not exist, create the directory and create a new store
         (_, false) => {
-            create_dir_all(&flush_path).with_context(|| "Failed to create home directory")?;
+            create_dir_all(flush_path).with_context(|| "Failed to create home directory")?;
             let db = RocksDbStore::new(flush_path)?;
             debug!("store created successfully at {:?}", &flush_path);
             db
