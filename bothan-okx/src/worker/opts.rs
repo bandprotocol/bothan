6c86de42e902d891d0806c2ba49c5a777c85042d
Warittorn Cheevachaipimol <warittorn@bandprotocol.com>
2025-04-04 18:15:45 +0700

    remove unused

--- bothan-okx/src/worker/opts.rs
+++ bothan-okx/src/worker/opts.rs
@@ -2,29 +2,20 @@ use serde::{Deserialize, Serialize};
 
 use crate::api::types::DEFAULT_URL;
 
-pub const DEFAULT_CHANNEL_SIZE: usize = 1000;
-
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct WorkerOpts {
     #[serde(default = "default_url")]
     pub url: String,
-    #[serde(default = "default_internal_ch_size")]
-    pub internal_ch_size: usize,
 }
 
 fn default_url() -> String {
     DEFAULT_URL.to_string()
 }
 
-fn default_internal_ch_size() -> usize {
-    DEFAULT_CHANNEL_SIZE
-}
-
 impl Default for WorkerOpts {
     fn default() -> Self {
         Self {
             url: default_url(),
-            internal_ch_size: default_internal_ch_size(),
         }
     }
 }
